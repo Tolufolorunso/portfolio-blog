c323ab985f664b735079c180e43b8bf0af5d61f7
Tolulope Folorunso <tolufolorunso@yahoo.com>
2021-01-14 01:38:12 +0100

    make things better

--- app.js
+++ app.js
@@ -1,80 +1,78 @@
-const cors = require("cors");
-const compression = require("compression");
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const { appTime, globalError } = require("./middlewares/middleware");
-const session = require("express-session");
-const MongoDBSession = require("connect-mongodb-session")(session);
-require("dotenv").config();
-
-const adminRoutes = require("./routes/adminRoutes");
-const blogRoutes = require("./routes/blogRoutes");
-const adminCrudRoutes = require("./routes/adminCrudRoutes");
+const cors = require('cors');
+const compression = require('compression');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const { appTime, globalError } = require('./middlewares/middleware');
+const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
+require('dotenv').config();
 
 const app = express();
 
 const store = new MongoDBSession({
-  uri:
-    "mongodb+srv://Tolulope:fyLY5ztoWrP1jF1e@cluster0-sdeee.mongodb.net/Tolu-blog",
-  collection: "sessions",
+	uri: process.env.DATABASE_SESSION,
+	collection: 'sessions'
 });
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
-  session({
-    secret: "my screct",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
+	session({
+		secret: 'my screct',
+		resave: false,
+		saveUninitialized: false,
+		store: store
+	})
 );
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static(`${__dirname}/public`));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Customs Middleware
 app.use(appTime);
 app.use(compression());
 
-app.use("/admin", adminRoutes);
-app.use("/blog", blogRoutes);
-app.use("/dashboard", adminCrudRoutes);
+const adminRoutes = require('./routes/adminRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const adminCrudRoutes = require('./routes/adminCrudRoutes');
+app.use('/admin', adminRoutes);
+app.use('/blog', blogRoutes);
+app.use('/dashboard', adminCrudRoutes);
 
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    title: "Tolu's personal page",
-    time: req.time,
-    isAuthenticated: false,
-  });
+app.get('/', (req, res) => {
+	res.status(200).render('index', {
+		title: "Tolu's personal page",
+		time: req.time,
+		isAuthenticated: false
+	});
 });
-app.get("*", (req, res) => {
-  res.status(200).render("404", {
-    title: "404 Page",
-    time: req.time,
-    isAuthenticated: false,
-  });
+app.get('*', (req, res) => {
+	res.status(200).render('404', {
+		title: '404 Page',
+		time: req.time,
+		isAuthenticated: false
+	});
 });
 
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then((c) => console.log("DATABASE connection successfull"))
-  .catch(() => console.log("not connected to db"));
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	})
+	.then(c => console.log('DATABASE connection successfull'))
+	.catch(() => console.log('not connected to db'));
 
 app.use(globalError);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  // console.log(`Server started on ${PORT}`);
+	// console.log(`Server started on ${PORT}`);
 });
