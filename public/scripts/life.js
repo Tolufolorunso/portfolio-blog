22519290c3ef2969c66ba839e403e1a6372aa65e
Tolulope Folorunso <tolufolorunso@yahoo.com>
2021-01-17 01:06:47 +0100

    fetch api bug

--- public/scripts/life.js
+++ public/scripts/life.js
@@ -8,19 +8,23 @@ const services = document.getElementById('services');
 
 const sendMessage = async message => {
 	try {
-		let data = await fetch('https://tolufolorunso.herokuapp.com/', {
+		let data = await fetch('/', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(message)
 		});
+		console.log(data);
 		if (!data.ok) {
 			throw Error('Something went wrong, please try again');
 		}
 		let res = await data.json();
+		console.log(data);
 		return res;
 	} catch (error) {
+		console.log(error);
+
 		return error;
 	}
 };
@@ -46,12 +50,13 @@ form.addEventListener('submit', ev => {
 
 	sendMessage(emailData)
 		.then(res => {
+			console.log(res);
 			msg.innerHTML = `<p>${res.title}</p>`;
 			submit.innerText = 'Submit';
 		})
 		.catch(error => {
 			console.log(error);
-			// msg.innerHTML = `<p style="color:red;">Something went wrong, please try again</p>`;
-			// submit.innerText = 'Submit';
+			msg.innerHTML = `<p style="color:red;">Something went wrong, please try again</p>`;
+			submit.innerText = 'Submit';
 		});
 });
