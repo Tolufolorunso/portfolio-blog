ee08326b79edbaa3e2bd5751b20ec26b043ab605
KolaTolulope <81151811+KolaTolulope@users.noreply.github.com>
2021-08-15 16:03:52 +0100

    bug fixes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const createAdmin = async function () {
 const app = express()
 
 const store = new MongoDBSession({
-  uri: process.env.DATABASE_SESSION_LOCAL,
+  uri: process.env.DATABASE_SESSION,
   collection: 'sessions'
 })
 // Middlewares
@@ -76,7 +76,7 @@ app.get('*', (req, res) => {
 })
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
